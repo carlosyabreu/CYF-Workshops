decb67ebe6ba3408829542aa6c881894df556d46
Carlos Abreu <carlos.abreu@zoho.com>
2025-11-08 11:03:06 +0000

    Add header.js and main.js files

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/header.js
@@ -0,0 +1,7 @@
+
+
+  const header = document.createElement("header");
+  header.innerHTML = `
+        <h0>Number Counter</h1>
+        <p>A simple counter. Press increment to increase the count by one.</p>
+    `;
